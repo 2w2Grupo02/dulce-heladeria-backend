a8f03d7a4d4e48df404a98ca9d8ba28bfb099ba3
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-10-29 22:10:46 -0300

    PG2022-163 migracion correccion

---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.DataAccess/Migrations/20221030010655_migracionInicial.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dulce.Heladeria.DataAccess.Migrations
+{
+    public partial class migracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deposit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deposit", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentifierType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentifierType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    Dni = table.Column<string>(maxLength: 100, nullable: false),
+                    UserName = table.Column<string>(maxLength: 100, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false),
+                    Password = table.Column<string>(maxLength: 100, nullable: false),
+                    Rol = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    BusinessName = table.Column<string>(nullable: false),
+                    IdentifierTypeId = table.Column<int>(nullable: false),
+                    Identifier = table.Column<string>(nullable: true),
+                    HomeAdress = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Client_IdentifierType_IdentifierTypeId",
+                        column: x => x.IdentifierTypeId,
+                        principalTable: "IdentifierType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ItemTypeId = table.Column<int>(nullable: false),
+                    MeasuringType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Item_ItemType_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "ItemType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Column = table.Column<string>(nullable: false),
+                    Row = table.Column<string>(nullable: false),
+                    Capacity = table.Column<int>(nullable: false),
+                    ItemTypeId = table.Column<int>(nullable: false),
+                    DepositId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Location_Deposit_DepositId",
+                        column: x => x.DepositId,
+                        principalTable: "Deposit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Location_ItemType_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "ItemType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemStock",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Amount = table.Column<int>(nullable: false),
+                    ItemId = table.Column<int>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false),
+                    EntryDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemStock", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemStock_Item_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Item",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemStock_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockMovement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeletionDate = table.Column<DateTime>(nullable: true),
+                    Amount = table.Column<int>(nullable: false),
+                    Motive = table.Column<string>(nullable: false),
+                    MovementDate = table.Column<DateTime>(nullable: false),
+                    ItemStockId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockMovement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockMovement_ItemStock_ItemStockId",
+                        column: x => x.ItemStockId,
+                        principalTable: "ItemStock",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_IdentifierTypeId",
+                table: "Client",
+                column: "IdentifierTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_ItemTypeId",
+                table: "Item",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemStock_ItemId",
+                table: "ItemStock",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemStock_LocationId",
+                table: "ItemStock",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_DepositId",
+                table: "Location",
+                column: "DepositId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_ItemTypeId",
+                table: "Location",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockMovement_ItemStockId",
+                table: "StockMovement",
+                column: "ItemStockId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "StockMovement");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "IdentifierType");
+
+            migrationBuilder.DropTable(
+                name: "ItemStock");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Deposit");
+
+            migrationBuilder.DropTable(
+                name: "ItemType");
+        }
+    }
+}
