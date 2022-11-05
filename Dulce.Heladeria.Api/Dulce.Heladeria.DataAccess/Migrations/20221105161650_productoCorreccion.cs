673cc052a37b139cd48e8120b6cfa19689a3f538
Jeremias Quevedo <112958@tecnicatura.frc.utn.edu.ar>
2022-11-05 18:33:23 -0300

    PG2022-176 correcciones agregar producto

---
+++ Dulce.Heladeria.Api/Dulce.Heladeria.DataAccess/Migrations/20221105161650_productoCorreccion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dulce.Heladeria.DataAccess.Migrations
+{
+    public partial class productoCorreccion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Product",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Product");
+        }
+    }
+}
